d698fcfba97c1e48e6a62a4fe155ef7f43b11164
Gj <gongjie0422@163.com>
2021-03-17 17:26:55 +0800

    fix: modify tagsGroup

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import SortableTable from "./components/Table/index";
 import HeadInfo from "./components/HeadInfo/index";
 import RouterForm from "./components/Router/index";
+import TagsGroup from "./components/TagsGroup";
 import { headerInfos } from "./utils/data";
 import { Button, Affix } from "antd";
 import { AlignCenterOutlined } from "@ant-design/icons";
@@ -16,6 +17,7 @@ export default class App extends React.Component {
       infos: { info: {} },
       isShow: false,
       list: [],
+      tags: [{ name: 'Default', focus: true, visible: false }]
     };
   }
 
@@ -26,6 +28,30 @@ export default class App extends React.Component {
   setRows(row) {
     const _list = this.state.list;
     this.setState({ list: [..._list, row] });
+  }
+
+  setTags(tagName) {
+    const { tags } = this.state
+    tags.push({ name: tagName, focus: true })
+    this.setState({ tags })
+  }
+
+  updateTagsStatus(index) {
+    const { tags } = this.state
+    tags[index].focus = !tags[index].focus
+    this.setState({ tags })
+  }
+
+  removeTag(index) {
+    const { tags } = this.state
+    tags.splice(index, 1)
+    this.setState({ tags })
+  }
+
+  tagVisibleChange(index, bool) {
+    const { tags } = this.state
+    tags[index].visible = bool
+    this.setState({ tags })
   }
 
   updateRows(rows) {
@@ -47,7 +73,17 @@ export default class App extends React.Component {
             <Button type="primary" style={{ marginLeft: 20 }}>Download</Button>
           </Affix>
           <HeadInfo infos={this.state.infos} />
-          <SortableTable updateRows={(list) => this.updateRows(list)} list={this.state.list} />
+          <TagsGroup
+            tags={this.state.tags}
+            setTags={(tags) => this.setTags(tags)}
+            updateTagsStatus={(index) => this.updateTagsStatus(index)}
+            removeTag={(index) => this.removeTag(index)}
+            tagVisibleChange={(index, bool) => this.tagVisibleChange(index, bool)}
+          />
+          <SortableTable
+            updateRows={(list) => this.updateRows(list)}
+            list={this.state.list}
+          />
           <Button
             size="small"
             type="ghost"
--- views/src/components/TagsGroup/index.jsx
+++ views/src/components/TagsGroup/index.jsx
@@ -1,14 +1,89 @@
-import { Button } from "antd";
 import React from "react";
-
+import { Button, Input, Popover } from "antd";
+import { BorderLeftOutlined, CheckSquareOutlined } from "@ant-design/icons";
+import "./index.scss"
 export default class TagsGroup extends React.Component {
 
+    inpRef = React.createRef(null)
+
+    state = {
+        isEdit: false,
+        tagName: '',
+    }
+
+    handleOk() {
+        if (this.state.tagName) this.props.setTags(this.state.tagName)
+        this.setState({ isEdit: false })
+    }
+
+    handleKeyDown(e) {
+        const { keyCode, target } = e
+        if (keyCode === 13 && target.value) {
+            this.props.setTags(target.value)
+            this.setState({ isEdit: false })
+            this.setState({ tagName: '' })
+        }
+    }
+
+    handleChange(e) {
+        this.setState({ tagName: e.target.value })
+    }
+
+    handleEdit() {
+        this.setState({ isEdit: true }, () => this.inpRef.current.focus())
+    }
+
+    // 选中
+    handleCheck(index) {
+        this.props.updateTagsStatus(index)
+    }
 
     render() {
         return (
-            <>
-                <Button></Button>
-            </>
+            <div className="tags-group-container">
+                {
+                    this.props.tags.map((tag, index) => {
+                        return <Popover
+                            size="small"
+                            content={<a onClick={() => this.props.removeTag(index)}>remove</a>}
+                            trigger="hover"
+                            visible={tag.visible}
+                            onVisibleChange={(bool) => this.props.tagVisibleChange(index, bool)}
+                        >
+                            <Button
+                                key={index}
+                                type={tag.focus && 'primary'}
+                                size="small"
+                                style={{ marginRight: 5 }}
+                                onClick={() => this.handleCheck(index)}
+                            >{tag.name}</Button>
+                        </Popover>
+                    })
+                }
+                {
+                    this.state.isEdit ?
+                        <>
+                            <Input
+                                ref={this.inpRef}
+                                size="small"
+                                onKeyDown={(e) => this.handleKeyDown(e)}
+                                placeholder="tag name"
+                                onChange={(e) => this.handleChange(e)}
+                                style={{ width: 80, marginRight: 5 }}
+                            />
+                            <Button
+                                size="small"
+                                onClick={() => this.handleOk()}
+                                icon={<CheckSquareOutlined style={{ color: '#F56C6C' }}
+                                />} />
+                        </> :
+                        <Button
+                            size="small"
+                            onClick={() => this.handleEdit()}
+                            icon={<BorderLeftOutlined style={{ color: '#F56C6C' }}
+                            />} />
+                }
+            </div>
         );
     }
 }
