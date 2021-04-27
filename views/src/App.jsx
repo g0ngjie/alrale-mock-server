82da3c3a2772c1d567823d8a37f744cba8f72090
Gj <gongjie0422@163.com>
2021-04-27 19:43:33 +0800

    :pencil2: 优化 标签自动换行行

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -9,7 +9,11 @@ import Condition from "./components/Router/Condition";
 import TagsGroup from "./components/TagsGroup";
 import { headerInfos } from "./utils/data";
 import { Button, Upload, message } from "antd";
-import { AlignCenterOutlined, UploadOutlined, DownloadOutlined } from "@ant-design/icons";
+import {
+  AlignCenterOutlined,
+  UploadOutlined,
+  DownloadOutlined,
+} from "@ant-design/icons";
 
 export default class App extends React.Component {
   state = {};
@@ -22,7 +26,7 @@ export default class App extends React.Component {
       isShow: false,
       list: [], // table 数据
       cacheTables: [], // 缓存table数据
-      tags: [{ name: 'Default', focus: true, visible: false }],
+      tags: [{ name: "Default", focus: true, visible: false }],
       routerValues: {},
       isRouterEdit: false,
       isCondition: false,
@@ -32,113 +36,140 @@ export default class App extends React.Component {
   }
 
   setModalShow(bool) {
-    this.setState({ isRouterEdit: false, isCondition: false, routerValues: {} }, () => {
-      this.setState({ isShow: bool })
-    });
+    this.setState(
+      { isRouterEdit: false, isCondition: false, routerValues: {} },
+      () => {
+        this.setState({ isShow: bool });
+      }
+    );
   }
 
   removeRow(index) {
-    const { list } = this.state
-    list.splice(index, 1)
-    const newList = list.map(item => {
+    const { list } = this.state;
+    list.splice(index, 1);
+    const newList = list.map((item) => {
       return {
         ...item,
-        index: item.index + Math.random().toString().substr(2, 3)
-      }
-    })
-    this.setState({ list: newList, cacheTables: newList })
+        index: item.index + Math.random().toString().substr(2, 3),
+      };
+    });
+    this.setState({ list: newList, cacheTables: newList });
   }
 
   editRow(index, isCondition = false) {
-    const { list } = this.state
-    this.setState({
-      routerValues: list[index],
-      isRouterEdit: !isCondition,
-      editRouterIndex: index
-    }, () => this.setState({ isShow: !isCondition, isCondition }))
+    const { list } = this.state;
+    this.setState(
+      {
+        routerValues: list[index],
+        isRouterEdit: !isCondition,
+        editRouterIndex: index,
+      },
+      () => this.setState({ isShow: !isCondition, isCondition })
+    );
   }
 
   // 修改row的code
   updateRowCodeByKey(key, code) {
-    const { routerValues } = this.state
-    routerValues[key] = code
-    routerValues.index = uniqueId()
-    this.setState({ routerValues })
+    const { routerValues } = this.state;
+    routerValues[key] = code;
+    routerValues.index = uniqueId();
+    this.setState({ routerValues });
   }
 
   setRows(row) {
-    const { list: _list, isRouterEdit, isCondition, editRouterIndex } = this.state;
-    row.remove = (index) => this.removeRow(index)
-    row.edit = (index) => this.editRow(index)
-    row.editCondition = (index) => this.editRow(index, true)
+    const {
+      list: _list,
+      isRouterEdit,
+      isCondition,
+      editRouterIndex,
+    } = this.state;
+    row.remove = (index) => this.removeRow(index);
+    row.edit = (index) => this.editRow(index);
+    row.editCondition = (index) => this.editRow(index, true);
     if (isRouterEdit) {
-      _list[editRouterIndex] = { ..._list[editRouterIndex], ...row }
-      const newList = _list.map(item => {
+      _list[editRouterIndex] = { ..._list[editRouterIndex], ...row };
+      const newList = _list.map((item) => {
         return {
           ...item,
-          index: uniqueId()
-        }
-      })
+          index: uniqueId(),
+        };
+      });
       this.setState({ list: newList, cacheTables: newList, routerValues: {} });
     } else if (isCondition) {
-      _list[editRouterIndex].condition = row.condition
-      const newList = _list.map(item => {
+      _list[editRouterIndex].condition = row.condition;
+      const newList = _list.map((item) => {
         return {
           ...item,
-          index: uniqueId()
-        }
-      })
+          index: uniqueId(),
+        };
+      });
       this.setState({ list: newList, cacheTables: newList, routerValues: {} });
-    } else this.setState({ list: [..._list, row], cacheTables: [..._list, row] });
+    } else
+      this.setState({ list: [..._list, row], cacheTables: [..._list, row] });
   }
 
   setTags(tagName) {
-    const { tags } = this.state
-    tags.push({ name: tagName, focus: true })
-    this.setState({ tags })
+    const { tags } = this.state;
+    tags.push({ name: tagName, focus: true });
+    this.setState({ tags });
+  }
+
+  // 清除全部选中条件
+  tagsClearAll() {
+    const { tags } = this.state;
+    tags.forEach((item) => (item.focus = false));
+    this.setState({ tags, list: [] });
   }
 
   updateTagsStatus(_, index) {
-    const { tags, cacheTables } = this.state
-    tags[index].focus = !tags[index].focus
+    const { tags, cacheTables } = this.state;
+    tags[index].focus = !tags[index].focus;
 
     // 过滤列表
-    const filterList = tags.filter(tag => tag.focus).map(tag => tag.name);
+    const filterList = tags.filter((tag) => tag.focus).map((tag) => tag.name);
     const newList = [];
-    cacheTables.forEach(row => {
+    cacheTables.forEach((row) => {
       if (filterList.includes(row.tag)) {
-        newList.push({ ...row, index: uniqueId() })
+        newList.push({ ...row, index: uniqueId() });
       }
-    })
-    this.setState({ list: newList, tags })
+    });
+    this.setState({ list: newList, tags });
   }
 
   removeTag(index) {
-    const { tags } = this.state
-    tags.splice(index, 1)
-    this.setState({ tags })
+    const { tags } = this.state;
+    tags.splice(index, 1);
+    this.setState({ tags });
   }
 
   tagVisibleChange(index, bool) {
-    const { tags } = this.state
-    tags[index].visible = bool
-    this.setState({ tags })
+    const { tags } = this.state;
+    tags[index].visible = bool;
+    this.setState({ tags });
   }
 
   updateRows(rows) {
-    this.setState({ list: rows })
+    this.setState({ list: rows });
   }
 
   setInfos(infos) {
-    this.setState({ infos: { ...this.state.infos, ...infos } })
+    this.setState({ infos: { ...this.state.infos, ...infos } });
   }
 
   /**下载 */
   handleDownload() {
-    const { infos, list } = this.state
-    const swagger = { ...infos, paths: {} }
-    list.forEach(row => {
-      const { path, method, tag, summary, parameters, responses, condition } = row
+    const { infos, list } = this.state;
+    const swagger = { ...infos, paths: {} };
+    list.forEach((row) => {
+      const {
+        path,
+        method,
+        tag,
+        summary,
+        parameters,
+        responses,
+        condition,
+      } = row;
       swagger.paths[path] = {
         [method]: {
           summary,
@@ -146,66 +177,82 @@ export default class App extends React.Component {
           produces: ["application/json"],
           parameters,
           responses,
-          condition
-        }
-      }
-    })
-    simpleDownload(`module.exports = ${JSON.stringify(swagger, null, '\t')}`, `${randomString(5).toLowerCase()}_mock.js`)
+          condition,
+        },
+      };
+    });
+    simpleDownload(
+      `module.exports = ${JSON.stringify(swagger, null, "\t")}`,
+      `${randomString(5).toLowerCase()}_mock.js`
+    );
   }
 
   handleBeforeUpload(file) {
-    this.setState({ loading: true })
+    this.setState({ loading: true });
     // 判断后缀
-    const flag = stringExtension(file.name, '.', 'js')
+    const flag = stringExtension(file.name, ".", "js");
     if (!flag) {
-      message.error('请上传js文件')
-      this.setState({ loading: false })
-      return Promise.reject()
+      message.error("请上传js文件");
+      this.setState({ loading: false });
+      return Promise.reject();
     }
-    return Promise.resolve()
+    return Promise.resolve();
   }
 
   handleUpload(info) {
-    if (info.file.status !== 'uploading') {
+    if (info.file.status !== "uploading") {
       // console.log(info.file, info.fileList);
     }
-    if (info.file.status === 'done') {
-      this.setState({ loading: false })
-      const { file } = info
-      const { ok, data } = file.response
+    if (info.file.status === "done") {
+      this.setState({ loading: false });
+      const { file } = info;
+      const { ok, data } = file.response;
       if (ok) {
         const { swagger, info, schemes, paths = {}, host, basePath } = data;
         const list = [];
-        const tagList = new Set()
+        const tagList = new Set();
         for (const path in paths) {
-          const router = { path, index: uniqueId() }
+          const router = { path, index: uniqueId() };
           if (Object.hasOwnProperty.call(paths, path)) {
             const item = paths[path];
             for (const method in item) {
               if (Object.hasOwnProperty.call(item, method)) {
-                const { tags, condition, parameters, responses, summary } = item[method];
-                router.summary = summary
-                router.method = method
-                router.condition = condition
-                router.parameters = parameters
-                router.responses = responses
-                router.tag = tags[0]
-                tagList.add(tags[0])
+                const {
+                  tags,
+                  condition,
+                  parameters,
+                  responses,
+                  summary,
+                } = item[method];
+                router.summary = summary;
+                router.method = method;
+                router.condition = condition;
+                router.parameters = parameters;
+                router.responses = responses;
+                router.tag = tags[0];
+                tagList.add(tags[0]);
               }
             }
           }
-          router.remove = (index) => this.removeRow(index)
-          router.edit = (index) => this.editRow(index)
-          router.editCondition = (index) => this.editRow(index, true)
-          list.push(router)
+          router.remove = (index) => this.removeRow(index);
+          router.edit = (index) => this.editRow(index);
+          router.editCondition = (index) => this.editRow(index, true);
+          list.push(router);
         }
-        const tags = []
-        tagList.forEach(tag => tags.push({ name: tag, focus: true, visible: false }))
-        this.setState({ infos: { swagger, info, schemes, host, basePath }, list, cacheTables: list, tags })
+        const tags = [];
+        tagList.forEach((tag) =>
+          tags.push({ name: tag, focus: true, visible: false })
+        );
+        this.setState({
+          infos: { swagger, info, schemes, host, basePath },
+          list,
+          cacheTables: list,
+          tags,
+        });
       }
-    } else if (info.file.status === 'error') {
-      console.log('error')
-      this.setState({ loading: false })
+    } else if (info.file.status === "error") {
+      console.log("error");
+      this.setState({ loading: false });
     }
   }
 
@@ -213,7 +260,9 @@ export default class App extends React.Component {
     return (
       <div className="container">
         <div className="container-columns">
-          <div style={{ display: 'flex', position: 'fixed', top: 25, right: 25 }}>
+          <div
+            style={{ display: "flex", position: "fixed", top: 25, right: 25 }}
+          >
             <Upload
               beforeUpload={(file) => this.handleBeforeUpload(file)}
               name="file"
@@ -246,9 +295,12 @@ export default class App extends React.Component {
             loading={this.state.loading}
             tags={this.state.tags}
             setTags={(tags) => this.setTags(tags)}
+            tagsClearAll={() => this.tagsClearAll()}
             updateTagsStatus={(tag, index) => this.updateTagsStatus(tag, index)}
             removeTag={(index) => this.removeTag(index)}
-            tagVisibleChange={(index, bool) => this.tagVisibleChange(index, bool)}
+            tagVisibleChange={(index, bool) =>
+              this.tagVisibleChange(index, bool)
+            }
           />
           <SortableTable
             loading={this.state.loading}
@@ -272,7 +324,9 @@ export default class App extends React.Component {
             isShow={this.state.isShow}
             isRouterEdit={this.state.isRouterEdit}
             routerValues={this.state.routerValues}
-            updateRowCodeByKey={(key, code) => this.updateRowCodeByKey(key, code)}
+            updateRowCodeByKey={(key, code) =>
+              this.updateRowCodeByKey(key, code)
+            }
           />
           <Condition
             setRows={(row) => this.setRows(row)}
@@ -280,7 +334,9 @@ export default class App extends React.Component {
             isShow={this.state.isCondition}
             isRouterEdit={this.state.isCondition}
             routerValues={this.state.routerValues}
-            updateRowCodeByKey={(key, code) => this.updateRowCodeByKey(key, code)}
+            updateRowCodeByKey={(key, code) =>
+              this.updateRowCodeByKey(key, code)
+            }
           />
         </div>
       </div>
--- views/src/components/TagsGroup/index.jsx
+++ views/src/components/TagsGroup/index.jsx
@@ -1,98 +1,121 @@
 import React from "react";
 import { Button, Input, Popover } from "antd";
-import { BorderLeftOutlined, CheckSquareOutlined } from "@ant-design/icons";
+import {
+  BorderLeftOutlined,
+  CheckSquareOutlined,
+  StopOutlined,
+} from "@ant-design/icons";
 
 export default class TagsGroup extends React.Component {
+  inpRef = React.createRef(null);
 
-    inpRef = React.createRef(null)
+  state = {
+    isEdit: false,
+    tagName: "",
+  };
 
-    state = {
-        isEdit: false,
-        tagName: '',
+  handleOk() {
+    if (this.state.tagName) this.props.setTags(this.state.tagName);
+    this.setState({ isEdit: false });
+  }
+
+  handleKeyDown(e) {
+    const { keyCode, target } = e;
+    if (keyCode === 13 && target.value) {
+      this.props.setTags(target.value);
+      this.setState({ isEdit: false, tagName: "" });
     }
+  }
 
-    handleOk() {
-        if (this.state.tagName) this.props.setTags(this.state.tagName)
-        this.setState({ isEdit: false })
-    }
+  handleBlur(e) {
+    const { target } = e;
+    if (target.value) this.props.setTags(target.value);
+    this.setState({ isEdit: false, tagName: "" });
+  }
 
-    handleKeyDown(e) {
-        const { keyCode, target } = e
-        if (keyCode === 13 && target.value) {
-            this.props.setTags(target.value)
-            this.setState({ isEdit: false, tagName: '' })
-        }
-    }
+  handleChange(e) {
+    this.setState({ tagName: e.target.value });
+  }
 
-    handleBlur(e) {
-        const { target } = e
-        if (target.value) this.props.setTags(target.value)
-        this.setState({ isEdit: false, tagName: '' })
-    }
+  handleEdit() {
+    this.setState({ isEdit: true }, () => this.inpRef.current.focus());
+  }
 
-    handleChange(e) {
-        this.setState({ tagName: e.target.value })
-    }
+  // 选中
+  handleCheck(tag, index) {
+    this.props.updateTagsStatus(tag, index);
+  }
 
-    handleEdit() {
-        this.setState({ isEdit: true }, () => this.inpRef.current.focus())
-    }
-
-    // 选中
-    handleCheck(tag, index) {
-        this.props.updateTagsStatus(tag, index)
-    }
-
-    render() {
-        return (
-            <div style={{ width: '100%', display: 'flex', marginBottom: '10px' }}>
-                {
-                    this.props.tags.map((tag, index) => {
-                        return <Popover
-                            key={index}
-                            size="small"
-                            content={<a onClick={() => this.props.removeTag(index)}>remove</a>}
-                            trigger="hover"
-                            visible={tag.visible}
-                            onVisibleChange={(bool) => this.props.tagVisibleChange(index, bool)}
-                        >
-                            <Button
-                                disabled={this.props.loading}
-                                type={tag.focus && 'primary'}
-                                size="small"
-                                style={{ marginRight: 5 }}
-                                onClick={() => this.handleCheck(tag, index)}
-                            >{tag.name}</Button>
-                        </Popover>
-                    })
-                }
-                {
-                    this.state.isEdit ?
-                        <>
-                            <Input
-                                ref={this.inpRef}
-                                size="small"
-                                onKeyDown={(e) => this.handleKeyDown(e)}
-                                onBlur={(e) => this.handleBlur(e)}
-                                placeholder="tag name"
-                                onChange={(e) => this.handleChange(e)}
-                                style={{ width: 80, marginRight: 5 }}
-                            />
-                            <Button
-                                disabled={this.props.loading}
-                                size="small"
-                                onClick={() => this.handleOk()}
-                                icon={<CheckSquareOutlined style={{ color: '#F56C6C' }}
-                                />} />
-                        </> :
-                        <Button
-                            disabled={this.props.loading}
-                            size="small"
-                            onClick={() => this.handleEdit()}
-                            icon={<BorderLeftOutlined style={{ color: '#F56C6C' }}
-                            />} />
-                }
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          marginBottom: "10px",
+          flexWrap: "wrap",
+        }}
+      >
+        {this.props.tags.map((tag, index) => {
+          return (
+            <Popover
+              key={index}
+              size="small"
+              content={
+                <a onClick={() => this.props.removeTag(index)}>remove</a>
+              }
+              trigger="hover"
+              visible={tag.visible}
+              onVisibleChange={(bool) =>
+                this.props.tagVisibleChange(index, bool)
+              }
+            >
+              <Button
+                disabled={this.props.loading}
+                type={tag.focus && "primary"}
+                size="small"
+                style={{ marginRight: 8, marginBottom: 8 }}
+                onClick={() => this.handleCheck(tag, index)}
+              >
+                {tag.name}
+              </Button>
+            </Popover>
+          );
+        })}
+        {this.state.isEdit ? (
+          <>
+            <Input
+              ref={this.inpRef}
+              size="small"
+              onKeyDown={(e) => this.handleKeyDown(e)}
+              onBlur={(e) => this.handleBlur(e)}
+              placeholder="tag name"
+              onChange={(e) => this.handleChange(e)}
+              style={{ width: 80, marginRight: 5 }}
+            />
+            <Button
+              disabled={this.props.loading}
+              size="small"
+              onClick={() => this.handleOk()}
+              icon={<CheckSquareOutlined style={{ color: "#F56C6C" }} />}
+            />
+          </>
+        ) : (
+          <Button
+            disabled={this.props.loading}
+            size="small"
+            onClick={() => this.handleEdit()}
+            icon={<BorderLeftOutlined style={{ color: "#F56C6C" }} />}
+          />
+        )}
+        <Button
+          disabled={this.props.loading}
+          size="small"
+          onClick={() => this.props.tagsClearAll()}
+          style={{ marginLeft: 8 }}
+          icon={<StopOutlined />}
+        />
+      </div>
+    );
+  }
 }
